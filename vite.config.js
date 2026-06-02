9fb6eed7be8b89b1a5918f123d8551808d8515a7
otoken <oooooooo996@qq.com>
2026-06-02 23:15:31 +0800

    pages部署

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'/glowing.github.io/',
   plugins: [
     vue(),
     AutoImport({
